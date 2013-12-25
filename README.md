f8767e1274bc404c6410823f61315b0ad60fab11
Joe R <transfix@sublevels.net>
2013-12-24 20:41:50 -0800

    A little cleanup and some documentation.

--- README.md
+++ README.md
@@ -1,6 +1,58 @@
 trans-btce
 ==========
 
-Command line interface to btc-e.com
+Basic command line interface to btc-e.com.
+If this is useful to you, consider a BTC tip: 1E5PdkFpee1rG694VpJgjm5h8b8fqWQtvm
+- Joe R - transfix@sublevels.net
 
-More documentation soon...
+Requires:
+php5-cli
+php5-curl
+
+Set the environment variables BTCE_KEY and BTCE_SECRET to your auth credentials.
+
+Usage:
+$ php btce.php <command> <command args>
+
+Commands:
+- balance <currency>
+  <currency> - btc/ltc/nmc etc.
+  Returns your balance for the specified currency.
+
+- server_time
+  Returns the current server time.
+
+- ticker <pair> [info]
+  <pair> - btc_usd, ltc_btc, etc.
+  [info] - high, low, vol, last, etc.
+  If [info] is not specified, defaults to 'last'
+
+- trades <pair>
+  <pair> - btc_usd, ltc_btc, etc.
+  Returns recent trades.
+
+- depth <pair>
+  <pair> - btc_usd, ltc_btc, etc.
+  Returns market depth.
+
+- TransHistory <start time> <end time>
+  <start time> - string specifing time as parsable by strtotime()
+  <end time>   - string specifing time as parsable by strtotime()
+
+- TradeHistory
+  <count> - number of items to show
+  Returns the specifed number of items in your trade history starting from most recent.
+
+- ActiveOrders
+  Returns currently active orders.
+
+- Trade <pair> <type> <rate> <amount>
+  <pair>   - btc_usd, ltc_btc, etc.
+  <type>   - 'buy' or 'sell'
+  <rate>   - rate to buy/sell
+  <amount> - amount to buy/sell at specified rate
+  Creates a trade order.
+
+- CancelOrder <order_id>
+  <order_id> - id of order you wish to cancel
+  Cancels specified order.
--- lib/btce_query.php
+++ lib/btce_query.php
@@ -1,243 +1,116 @@
 <?php
-//from https://btc-e.com/api/documentation
+//Using https://btc-e.com/api/documentation
 //transfix@sublevels.net 11/30/2013
 //11/30/2013 - adding a nonce offset to avoid issues with rapid queries 
 //12/23/2013 - adding decode flags
+//12/24/2013 - cleaning up a bit
 
-function btce_query($method, array $req = array(), $decode = true) {
-        // API settings
-        $key = getenv("BTCE_KEY"); // your API-key
-        if($key === false) throw new Exception('Need to set your API key (env var BTCE_KEY)');
-        $secret = getenv("BTCE_SECRET"); // your Secret-key
-        if($key === false) throw new Exception('Need to set your secret key (env var BTCE_SECRET)');
+function curl_req($url, $headers = false, $post_data = false, $decode = true)
+{
+  // our curl handle (initialize if required)
+  static $ch = null;
+  if(is_null($ch)) 
+  {
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/4.0 (compatible; BTCE PHP client; '.php_uname('s').'; PHP/'.phpversion().')');
+  }
+  curl_setopt($ch, CURLOPT_URL, $url);
+  if($post_data != false)
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $post_data);
+  if($headers != false)
+    curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+  curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
  
-        $req['method'] = $method;
-        $mt = explode(' ', microtime());
-        $req['nonce'] = $mt[1];
+  // run the query
+  $res = curl_exec($ch);
+  if($res === false) throw new Exception('Could not get reply: '.curl_error($ch));
+  if(!$decode) return $res;
+  $dec = json_decode($res, true);
+  if (!$dec) throw new Exception('Invalid data received, please make sure connection is working and requested API exists');
+  return $dec;  
+}
 
-        static $nonce_offset = 0;
-        $req['nonce']+=$nonce_offset;
-        $nonce_offset++;
+function btce_query($method, array $req = array(), $decode = true)
+{
+  // API settings
+  $key = getenv("BTCE_KEY"); // your API-key
+  if($key === false) throw new Exception('Need to set your API key (env var BTCE_KEY)');
+  $secret = getenv("BTCE_SECRET"); // your Secret-key
+  if($key === false) throw new Exception('Need to set your secret key (env var BTCE_SECRET)');
+ 
+  $req['method'] = $method;
+  $mt = explode(' ', microtime());
+  $req['nonce'] = $mt[1];
+
+  static $nonce_offset = 0;
+  $req['nonce']+=$nonce_offset;
+  $nonce_offset++;
        
-        // generate the POST data string
-        $post_data = http_build_query($req, '', '&');
+  // generate the POST data string
+  $post_data = http_build_query($req, '', '&');
+
+  $sign = hash_hmac('sha512', $post_data, $secret);
  
-        $sign = hash_hmac('sha512', $post_data, $secret);
+  // generate the extra headers
+  $headers = array('Sign: '.$sign,'Key: '.$key);
  
-        // generate the extra headers
-        $headers = array(
-			'Sign: '.$sign,
-			'Key: '.$key,
-        );
- 
-        // our curl handle (initialize if required)
-        static $ch = null;
-        if (is_null($ch)) {
-                $ch = curl_init();
-                curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-                curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/4.0 (compatible; BTCE PHP client; '.php_uname('s').'; PHP/'.phpversion().')');
-        }
-        curl_setopt($ch, CURLOPT_URL, 'https://btc-e.com/tapi/');
-        curl_setopt($ch, CURLOPT_POSTFIELDS, $post_data);
-        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
- 
-        // run the query
-        $res = curl_exec($ch);
-        if ($res === false) throw new Exception('Could not get reply: '.curl_error($ch));
-        if(!$decode) return $res;
-        $dec = json_decode($res, true);
-        if (!$dec) throw new Exception('Invalid data received, please make sure connection is working and requested API exists');
-        return $dec;
+  $url = 'https://btc-e.com/tapi/';
+  return curl_req($url, $headers, $post_data, $decode);
 }
 
 function btce_btcusd_ticker($decode = true)
 {
-        // our curl handle (initialize if required)
-        static $ch = null;
-        if (is_null($ch)) {
-                $ch = curl_init();
-                curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-                curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/4.0 (compatible; BTCE PHP client; '.php_uname('s').'; PHP/'.phpversion().')');
-        }
-        curl_setopt($ch, CURLOPT_URL, 'https://btc-e.com/api/2/btc_usd/ticker');
-        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
- 
-        // run the query
-        $res = curl_exec($ch);
-        if ($res === false) throw new Exception('Could not get reply: '.curl_error($ch));
-        if(!$decode) return $res;
-        $dec = json_decode($res, true);
-        if (!$dec) throw new Exception('Invalid data received, please make sure connection is working and requested API exists');
-        return $dec;
+  $url = 'https://btc-e.com/api/2/btc_usd/ticker';
+  return curl_req($url, false, false, $decode);
 }
 
 function btce_ltcbtc_ticker($decode = true)
 {
-        // our curl handle (initialize if required)
-        static $ch = null;
-        if (is_null($ch)) {
-                $ch = curl_init();
-                curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-                curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/4.0 (compatible; BTCE PHP client; '.php_uname('s').'; PHP/'.phpversion().')');
-        }
-        curl_setopt($ch, CURLOPT_URL, 'https://btc-e.com/api/2/ltc_btc/ticker');
-        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
- 
-        // run the query
-        $res = curl_exec($ch);
-        if ($res === false) throw new Exception('Could not get reply: '.curl_error($ch));
-        if(!$decode) return $res;
-        $dec = json_decode($res, true);
-        if (!$dec) throw new Exception('Invalid data received, please make sure connection is working and requested API exists');
-        return $dec;
+  $url = 'https://btc-e.com/api/2/ltc_btc/ticker';
+  return curl_req($url, false, false, $decode);
 }
 
 function btce_ltcusd_ticker($decode = true)
 {
-        // our curl handle (initialize if required)
-        static $ch = null;
-        if (is_null($ch)) {
-                $ch = curl_init();
-                curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-                curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/4.0 (compatible; BTCE PHP client; '.php_uname('s').'; PHP/'.phpversion().')');
-        }
-        curl_setopt($ch, CURLOPT_URL, 'https://btc-e.com/api/2/ltc_usd/ticker');
-        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
- 
-        // run the query
-        $res = curl_exec($ch);
-        if ($res === false) throw new Exception('Could not get reply: '.curl_error($ch));
-        if(!$decode) return $res;
-        $dec = json_decode($res, true);
-        if (!$dec) throw new Exception('Invalid data received, please make sure connection is working and requested API exists');
-        return $dec;
+  $url = 'https://btc-e.com/api/2/ltc_usd/ticker';
+  return curl_req($url, false, false, $decode);
 }
 
 function btce_btcusd_trades($decode = true)
 {
-        // our curl handle (initialize if required)
-        static $ch = null;
-        if (is_null($ch)) {
-                $ch = curl_init();
-                curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-                curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/4.0 (compatible; BTCE PHP client; '.php_uname('s').'; PHP/'.phpversion().')');
-        }
-        curl_setopt($ch, CURLOPT_URL, 'https://btc-e.com/api/2/btc_usd/trades');
-        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
- 
-        // run the query
-        $res = curl_exec($ch);
-        if ($res === false) throw new Exception('Could not get reply: '.curl_error($ch));
-        if(!$decode) return $res;
-        $dec = json_decode($res, true);
-        if (!$dec) throw new Exception('Invalid data received, please make sure connection is working and requested API exists');
-        return $dec;
+  $url = 'https://btc-e.com/api/2/btc_usd/trades';
+  return curl_req($url, false, false, $decode);
 }
 
 function btce_ltcbtc_trades($decode = true)
 {
-        // our curl handle (initialize if required)
-        static $ch = null;
-        if (is_null($ch)) {
-                $ch = curl_init();
-                curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-                curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/4.0 (compatible; BTCE PHP client; '.php_uname('s').'; PHP/'.phpversion().')');
-        }
-        curl_setopt($ch, CURLOPT_URL, 'https://btc-e.com/api/2/ltc_btc/trades');
-        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
- 
-        // run the query
-        $res = curl_exec($ch);
-        if ($res === false) throw new Exception('Could not get reply: '.curl_error($ch));
-        if(!$decode) return $res;
-        $dec = json_decode($res, true);
-        if (!$dec) throw new Exception('Invalid data received, please make sure connection is working and requested API exists');
-        return $dec;
+  $url = 'https://btc-e.com/api/2/ltc_btc/trades';
+  return curl_req($url, false, false, $decode);
 }
 
 function btce_ltcusd_trades($decode = true)
 {
-        // our curl handle (initialize if required)
-        static $ch = null;
-        if (is_null($ch)) {
-                $ch = curl_init();
-                curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-                curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/4.0 (compatible; BTCE PHP client; '.php_uname('s').'; PHP/'.phpversion().')');
-        }
-        curl_setopt($ch, CURLOPT_URL, 'https://btc-e.com/api/2/ltc_usd/trades');
-        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
- 
-        // run the query
-        $res = curl_exec($ch);
-        if ($res === false) throw new Exception('Could not get reply: '.curl_error($ch));
-        if(!$decode) return $res;
-        $dec = json_decode($res, true);
-        if (!$dec) throw new Exception('Invalid data received, please make sure connection is working and requested API exists');
-        return $dec;
+  $url = 'https://btc-e.com/api/2/ltc_usd/trades';
+  return curl_req($url, false, false, $decode);
 }
  
 function btce_btcusd_depth($decode = true)
 {
-        // our curl handle (initialize if required)
-        static $ch = null;
-        if (is_null($ch)) {
-                $ch = curl_init();
-                curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-                curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/4.0 (compatible; BTCE PHP client; '.php_uname('s').'; PHP/'.phpversion().')');
-        }
-        curl_setopt($ch, CURLOPT_URL, 'https://btc-e.com/api/2/btc_usd/depth');
-        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
- 
-        // run the query
-        $res = curl_exec($ch);
-        if ($res === false) throw new Exception('Could not get reply: '.curl_error($ch));
-        if(!$decode) return $res;
-        $dec = json_decode($res, true);
-        if (!$dec) throw new Exception('Invalid data received, please make sure connection is working and requested API exists');
-        return $dec;
+  $url = 'https://btc-e.com/api/2/btc_usd/depth';
+  return curl_req($url, false, false, $decode);
 }
 
 function btce_ltcbtc_depth($decode = true)
 {
-        // our curl handle (initialize if required)
-        static $ch = null;
-        if (is_null($ch)) {
-                $ch = curl_init();
-                curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-                curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/4.0 (compatible; BTCE PHP client; '.php_uname('s').'; PHP/'.phpversion().')');
-        }
-        curl_setopt($ch, CURLOPT_URL, 'https://btc-e.com/api/2/ltc_btc/depth');
-        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
- 
-        // run the query
-        $res = curl_exec($ch);
-        if ($res === false) throw new Exception('Could not get reply: '.curl_error($ch));
-        if(!$decode) return $res;
-        $dec = json_decode($res, true);
-        if (!$dec) throw new Exception('Invalid data received, please make sure connection is working and requested API exists');
-        return $dec;
+  $url = 'https://btc-e.com/api/2/ltc_btc/depth';
+  return curl_req($url, false, false, $decode);
 }
 
 function btce_ltcusd_depth($decode = true)
 {
-        // our curl handle (initialize if required)
-        static $ch = null;
-        if (is_null($ch)) {
-                $ch = curl_init();
-                curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-                curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/4.0 (compatible; BTCE PHP client; '.php_uname('s').'; PHP/'.phpversion().')');
-        }
-        curl_setopt($ch, CURLOPT_URL, 'https://btc-e.com/api/2/ltc_usd/depth');
-        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
- 
-        // run the query
-        $res = curl_exec($ch);
-        if ($res === false) throw new Exception('Could not get reply: '.curl_error($ch));
-        if(!$decode) return $res;
-        $dec = json_decode($res, true);
-        if (!$dec) throw new Exception('Invalid data received, please make sure connection is working and requested API exists');
-        return $dec;
+  $url = 'https://btc-e.com/api/2/ltc_usd/depth';
+  return curl_req($url, false, false, $decode);
 }
 
 ?>
